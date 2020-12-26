ae54a45a8502ff60144dacd72f68e75da7aa79a5
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-26 13:53:22 -0300

    Adicionado PageHeader em LandingPage

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View } from 'react-native'
+import PageHeader from '../../components/PageHeader'
 
 function LandingPage() {
     return (
         <View>
-
+            <PageHeader title="Monsters List"></PageHeader>
         </View>
     )
 }
