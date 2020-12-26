2aaaf509345e26b9abf6e2e9af58df7eee6623bb
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-26 16:16:36 -0300

    Adicionado estilização em LandingPage

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { View } from 'react-native'
 import PageHeader from '../../components/PageHeader'
 
+import styles from './styles'
+
 function LandingPage() {
     return (
-        <View>
+        <View style={styles.container}>
             <PageHeader title="Monsters List"></PageHeader>
         </View>
     )
