65b64129d76bce87d72b6eff340bef3a77657192
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-26 18:15:20 -0300

    Adicionado chamada para api

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useEffect, useState }  from 'react'
+import { View, Text } from 'react-native'
 import PageHeader from '../../components/PageHeader'
+import api from '../../services/api'
 
 import styles from './styles'
 
 function LandingPage() {
+
+    const [references, setReferences] = useState([])
+
+    useEffect(() => {
+        api.get('monsters/').then( response => {
+            setReferences(response.data.results)
+        }).catch( error => 
+            console.log(error) 
+        )
+    }, [])
+
     return (
         <View style={styles.container}>
             <PageHeader title="Monsters List"></PageHeader>
