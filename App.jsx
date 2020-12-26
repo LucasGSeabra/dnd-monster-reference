565ebb18097162f31b35ccf7791cfd8b2165fa20
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-26 16:14:47 -0300

    Removido View

--- App.jsx
+++ App.jsx
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
 import AppStack from './src/routes/AppStack';
 
 export default function App() {
   return (
-    <View>
+    <>
       <AppStack />
       <StatusBar style="light" />
-    </View>
+    </>
   );
 }
