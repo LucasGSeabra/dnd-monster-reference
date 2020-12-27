de8600e17b342d3b7532c2c118ce60168bb75f3c
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-27 15:51:02 -0300

    Criado estiliza;ção para ReferenceItem

---
+++ src/components/ReferenceItem/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    item: {
+        width: '100%',
+        backgroundColor: '#fff',
+        marginVertical: 1
+    },
+    
+    itemName: {
+        fontSize: 18,
+        paddingLeft: 15,
+        marginVertical: 8
+    }
+})
+
+export default styles
