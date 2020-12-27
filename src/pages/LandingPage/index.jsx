f115d9a583a7b02bfca1303f94e325f72976b711
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-27 16:01:08 -0300

    Modificado LandingPage para exibir referencesList

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState }  from 'react'
 import { View, Text } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import PageHeader from '../../components/PageHeader'
+import ReferenceItem from '../../components/ReferenceItem'
 import api from '../../services/api'
 
 import styles from './styles'
 
 function LandingPage() {
 
-    const [references, setReferences] = useState([])
+    const [referencesList, setReferencesList] = useState([])
 
     useEffect(() => {
         api.get('monsters/').then( response => {
             setReferences(response.data.results)
+            setReferencesList(response.data.results)
         }).catch( error => 
             console.log(error) 
         )
@@ -20,6 +23,13 @@ function LandingPage() {
     return (
         <View style={styles.container}>
             <PageHeader title="Monsters List"></PageHeader>
+            <ScrollView>
+                { referencesList.map(reference => {
+                    return (
+                        <ReferenceItem name={reference.name} key={reference.index}/>
+                    )
+                })}
+            </ScrollView>
         </View>
     )
 }
