749f4ca02a17533006358956b1bf4bd648d8ca11
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-27 19:48:46 -0300

    Adicionado função handleClick

--- src/components/ReferenceItem/index.jsx
+++ src/components/ReferenceItem/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Text } from 'react-native'
 import { BorderlessButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles'
 
 function ReferenceItem(props) {
+
+    const { navigate } = useNavigation()
+
+    function handleClick(index) {
+        navigate('Reference', { index: index})
+    }
+
     return (
-        <BorderlessButton style={styles.item}>
+        <BorderlessButton style={styles.item} onPress={() => handleClick(props.index)}>
             <Text style={styles.itemName}>{props.name}</Text>
         </BorderlessButton>
     )
