26ba2b37e520c57c75a8d0cbc2065506445357ce
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-27 20:05:39 -0300

    Adicionado PageHeader

--- src/pages/ReferencePage/index.jsx
+++ src/pages/ReferencePage/index.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
+import PageHeader from '../../components/PageHeader'
+import api from '../../services/api'
 
 
 function ReferencePage(props) {
+
+    const [reference, setReference] = useState([])
+
+    useEffect(() => {
+        api.get(`${props.route.params.index}`).then( response => {
+            setReference(response.data)
+        }).catch( error => 
+            console.log(error) 
+        )
+    }, [])
     
     return (
         <View>
+            <PageHeader title={reference.name} subTitle={`${reference.size} ${reference.type}`} >
 
+            </PageHeader>
         </View>
     )
 }
