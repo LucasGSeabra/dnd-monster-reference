7f7fdb600b48a89d53dcbec658be6d8bfe2e920b
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-12-27 19:49:45 -0300

    Adicionado ReferencePage em AppStack

--- src/routes/AppStack.jsx
+++ src/routes/AppStack.jsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import LandingPage from '../pages/LandingPage'
+import ReferencePage from '../pages/ReferencePage'
 
 const { Navigator, Screen } = createStackNavigator()
 
@@ -11,6 +12,7 @@ function AppStack() {
         <NavigationContainer>
             <Navigator initialRouteName="Landing" screenOptions={{ headerShown: false }}>
                 <Screen name="Landing" component={LandingPage} />
+                <Screen name="Reference" component={ReferencePage} />
             </Navigator>
         </NavigationContainer>
     )
