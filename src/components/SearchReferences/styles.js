edf78fff4b7023415f9dc1c936752fbe3c911114
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-04 14:13:00 -0300

    Criado estilização para SearchReference

---
+++ src/components/SearchReferences/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+
+    },
+
+    searchInput: {
+        backgroundColor: '#fdcfce',
+        width: '70%',
+        height: 32, 
+        borderRadius: 8,
+        paddingHorizontal: 8
+    }
+})
+
+export default styles
