254070caa693b8142356559ed3009164a4abeab0
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-04 14:12:44 -0300

    Criado componente SearchReference

--- src/components/SearchReferences/index.jsx
+++ src/components/SearchReferences/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, TextInput } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
+
+import styles from './styles'
 
 function SearchReferences(props) {
+    const [searchInput, setSearchInput] = useState('')
+
+    function handleSearch(searchValue) {
+        setSearchInput(searchValue)
+        props.search(searchValue)
+    }
+
     return(
-        <View>
-            
+        <View style={styles.container} >
+            <Feather name="search" size={24} color="#fff"/>
+            <TextInput
+                style={styles.searchInput} 
+                clearButtonMode="always"
+                value={searchInput}
+                onChangeText={(text) => handleSearch(text)}
+            />
         </View>
     )
 }
