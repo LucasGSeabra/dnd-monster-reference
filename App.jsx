5ae7c4a5873bbd98462bd071e71ae654e35df327
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-06 11:54:53 -0300

    Modificado App para aguardar fontes

--- App.jsx
+++ App.jsx
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import AppStack from './src/routes/AppStack';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import AppStack from './src/routes/AppStack'
+import AppLoading from 'expo-app-loading'
+import { Montserrat_400Regular, Montserrat_600SemiBold, Montserrat_700Bold, useFonts } from '@expo-google-fonts/montserrat'
+import { VollkornSC_600SemiBold, VollkornSC_700Bold } from '@expo-google-fonts/vollkorn-sc'
 
 export default function App() {
-  return (
-    <>
-      <AppStack />
-      <StatusBar style="light" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    VollkornSC_600SemiBold,
+    VollkornSC_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    )
+  }  
 }
