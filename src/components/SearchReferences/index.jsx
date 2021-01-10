146948ebf28a17a90b918cc52e328b559fcce513
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2021-01-10 17:03:07 -0300

    Modificado handleSearch

--- src/components/SearchReferences/index.jsx
+++ src/components/SearchReferences/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, TextInput } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
@@ -8,19 +8,22 @@ import styles from './styles'
 function SearchReferences(props) {
     const [searchInput, setSearchInput] = useState('')
 
-    function handleSearch(searchValue) {
+    async function handleSearch(searchValue) {
         setSearchInput(searchValue)
+        await props.setReferencesList([])
+        await props.setPage(1)
+        await props.search(searchValue)
         props.search(searchValue)
     }
 
     return(
         <View style={styles.container} >
             <Feather name="search" size={24} color="#fff"/>
-            <TextInput
-                style={styles.searchInput} 
-                clearButtonMode="always"
-                value={searchInput}
-                onChangeText={(text) => handleSearch(text)}
+                <TextInput
+                    style={styles.searchInput} 
+                    clearButtonMode="always"
+                    value={searchInput}
+                    onChangeText={(text) => handleSearch(text)}
             />
         </View>
     )
